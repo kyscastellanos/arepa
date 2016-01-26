567de5cad532faa2143bb1e684aaf95ae7f04b51
Kysbel <kyscastellanos@gmail.com>
2016-01-26 19:55:28 +1100

    Theme test

---
+++ wp-content/themes/arepa/functions.php
@@ -0,0 +1,20 @@
+<?php
+
+add_filter('show_admin_bar', '__return_false');
+
+function theme_enqueue_style()
+{
+    wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() .'/css/bootstrap.min.css', false );
+    wp_enqueue_style( 'theme-css', get_template_directory_uri() .'/style.css', false );
+}
+
+function theme_enqueue_script()
+{
+    wp_enqueue_script( 'jquery-js', get_template_directory_uri() . '/js/jquery-2.1.4.min.js', [], '', true );
+    wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', [], '', true );
+    wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app.js', [], '', true );
+    wp_enqueue_script( 'parallax-js', get_template_directory_uri() . '/js/parallax.min.js', [], '', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_style' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_script' );
---
+++ wp-content/themes/arepa/index.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Test</h1>
+                <br><br><br><br>
+                <br><br><br><br>
+                <br><br><br><br>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="/wp-content/themes/arepa/img/parallax-test.jpg" data-natural-width="1400" data-natural-height="900"></div>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Test</h1>
+                <br><br><br><br>
+                <br><br><br><br>
+                <br><br><br><br>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="/wp-content/themes/arepa/img/parallax-test.jpg" data-natural-width="1400" data-natural-height="900"></div>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <h1>Test</h1>
+                <br><br><br><br>
+                <br><br><br><br>
+                <br><br><br><br>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
