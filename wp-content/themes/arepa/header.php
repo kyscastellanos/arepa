0f6221973ed177b09c89cbe5021087c188399875
Kysbel <kyscastellanos@gmail.com>
2016-02-13 12:05:17 +1100

    Draft

--- wp-content/themes/arepa/header.php
+++ wp-content/themes/arepa/header.php
@@ -4,10 +4,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title><?= get_bloginfo('name'); ?></title>
-        <link href='https://fonts.googleapis.com/css?family=Montserrat:700' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Cardo' rel='stylesheet' type='text/css'>
+
+        <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Hind' rel='stylesheet' type='text/css'>
+
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
         <?php wp_head(); ?>
     </head>
     <body>
--- wp-content/themes/arepa/index.php
+++ wp-content/themes/arepa/index.php
@@ -1,46 +1,117 @@
 <?php get_header(); ?>
 
-<section>
+
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10"
+     data-image-src="/wp-content/themes/arepa/img/arepa.jpg" data-natural-width="1270" data-natural-height="848">
+
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <h1>Test</h1>
-                <br><br><br><br>
-                <br><br><br><br>
-                <br><br><br><br>
+        <div class="row" style="margin-top: 40px;">
+            <div class="col-md-4 col-sm-6 col-xs-12">
+                <img src="/wp-content/themes/arepa/img/logo.png" class="img-responsive">
+            </div>
+
+            <div class="col-md-4 col-md-offset-4 col-sm-6 col-xs-12">
+                <p style="text-align: right; font-family: 'Montserrat'; font-size: 26px;">
+                    <a href="tel:+61415555666" style="color: black;">+61415555666</a>
+                    <br>
+                    <a href="mailto:hello@arepa.com.au" style="color: black;">hello@arepa.com.au</a>
+                </p>
+            </div>
+
+            <div class="col-xs-12" style="margin-top: 60px;">
+                <h1 style="text-align: center; font-family: 'Montserrat'; font-size: 46px;">
+                    The best thing since the taco!
+                </h1>
+            </div>
+
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3" style="margin-top: 30px; font-family: 'Montserrat'; text-align: center;">
+                <h2 style="font-size: 32px;">
+                    Get a FREE drink with your next purchase
+                </h2>
+
+                <br>
+                <br>
+
+                <div class="input-group input-group-lg">
+                    <input type="text" class="form-control" placeholder="E-mail address">
+                    <span class="input-group-btn">
+                        <button class="btn btn-success" type="button">Get voucher</button>
+                    </span>
+                </div>
+
+                <br>
+                <br>
+                <br>
+                <br>
+
             </div>
         </div>
     </div>
-</section>
+</div>
 
-<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="/wp-content/themes/arepa/img/parallax-test.jpg" data-natural-width="1400" data-natural-height="900"></div>
+<div class="container">
+    <div class="row" style="margin: 60px 0; text-align: center;">
+        <div class="col-xs-12">
+            <img src="/wp-content/themes/arepa/img/camion.png" class="img-responsive">
+        </div>
+    </div>
+</div>
 
-<section>
+<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10"
+     data-image-src="/wp-content/themes/arepa/img/arepa.jpg" data-natural-width="1270" data-natural-height="848">
+
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <h1>Test</h1>
-                <br><br><br><br>
-                <br><br><br><br>
-                <br><br><br><br>
+        <div class="row" style="margin-top: 40px;">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3" style="margin-top: 60px; font-family: 'Montserrat'; text-align: center; color: white;">
+                <h2 style="font-size: 32px;">
+                    Would you like to know the next time we bring arepas to your area?
+                </h2>
+
+                <br>
+                <br>
+
+                <div class="input-group input-group-lg">
+                    <input type="text" class="form-control" placeholder="E-mail address">
+                    <span class="input-group-btn">
+                        <button class="btn btn-success" type="button">Let me know</button>
+                    </span>
+                </div>
+
+                <br>
+                <br>
+                <br>
+                <br>
+
             </div>
         </div>
     </div>
-</section>
+</div>
 
-<div class="parallax-container" data-parallax="scroll" data-position="top" data-bleed="10" data-image-src="/wp-content/themes/arepa/img/parallax-test.jpg" data-natural-width="1400" data-natural-height="900"></div>
+<div class="container">
+    <div class="row" style="margin: 60px 0; text-align: center;">
+        <div class="col-xs-12">
+            <img src="/wp-content/themes/arepa/img/arepa.png" width="600px">
+        </div>
+    </div>
+</div>
 
-<section>
-    <div class="container">
-        <div class="row">
+    <div class="container-fluid">
+        <div class="row" style="background-color: darkgrey;">
             <div class="col-xs-12">
-                <h1>Test</h1>
-                <br><br><br><br>
-                <br><br><br><br>
-                <br><br><br><br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
+                <br>
             </div>
         </div>
     </div>
-</section>
 
 <?php get_footer(); ?>
