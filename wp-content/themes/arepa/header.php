e749978f14a2037f7839d3bba5a881192094a010
Kysbel <kyscastellanos@gmail.com>
2016-05-16 19:36:44 +1000

    Favicon fix

--- wp-content/themes/arepa/header.php
+++ wp-content/themes/arepa/header.php
@@ -14,7 +14,7 @@
         <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
         <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
-        <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/img/favicon.png"/>
+        <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico"/>
 
         <!--<meta name="p:domain_verify" content="561371a459af998ac401ad6f7b5a917d”/>-->
 
