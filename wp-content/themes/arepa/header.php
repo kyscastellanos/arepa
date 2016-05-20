db40e70f29099aea26cdafd47496419eecfda4b4
Kysbel <kyscastellanos@gmail.com>
2016-05-20 15:01:10 +1000

    GA tag

--- wp-content/themes/arepa/header.php
+++ wp-content/themes/arepa/header.php
@@ -4,8 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="">
-        <meta name="author" content="">
 
         <title>Arepa</title>
 
@@ -15,6 +13,17 @@
         <link href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css">
 
         <link rel="shortcut icon" type="image/png" href="<?php bloginfo('template_directory'); ?>/img/favicon.ico"/>
+
+        <!-- GA -->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-77840343-1', 'auto');
+            ga('send', 'pageview');
+        </script>
 
         <!--<meta name="p:domain_verify" content="561371a459af998ac401ad6f7b5a917d”/>-->
 
